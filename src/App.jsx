5924302fda45e6ca57705151dbd2913719b308be
Ttecs <tharakahalkewelatecs@gmail.com>
2023-03-26 14:46:26 +0530

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import StudentPage from "./views/studentPage/StudentPage";
+
+function App() {
+  return (
+    <div>
+      <StudentPage />
+    </div>
+  );
+}
+
+export default App;
